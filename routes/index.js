e41bb6bc8e7985adedc7987c7b8136434c8bfd70
PedroBauer <pgm.bauer@gmail.com>
2018-08-11 23:02:07 +0200

    Added some placeholders to the skills sections; Changed the Social icons; Placing more placeholders till proper text is prepared;

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,19 @@ router.get('/', function(req, res, next) {
   res.render('index', {page:'Home', menuId:'home'});
 });
 
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', {page:'Portfolio', menuId:'portfolio'});
+});
+
+router.get('/resume', function(req, res, next) {
+  res.render('resume', {page:'Resume', menuId:'resume'});
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {page:'About', menuId:'about'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {page:'Contact', menuId:'contact'});
+});
 module.exports = router;
